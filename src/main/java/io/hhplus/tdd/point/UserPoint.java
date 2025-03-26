cd65c004891c39850c95d05a2452ba8105c92eb8
DESKTOP-5JPIU0I\USER <repaid11010@naver.com>
2025-03-27 00:52:59 +0900

    add : UserPoint 도메인 보강 및 테스트 추가 완료

--- src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -18,7 +18,7 @@ public record UserPoint(
     	Long charger = this.point + amount;
     	
     	if (charger >= 1000000) {
-    		throw new IllegalArgumentException("최대 포인트를 넘겼습니다.");
+    		throw new IllegalArgumentException("최대 저장 가능한 포인트가 넘었습니다.");
     	}
     	
     	return new UserPoint(this.id, charger, System.currentTimeMillis());
@@ -29,10 +29,14 @@ public record UserPoint(
     }
     
     public UserPoint usePoint(Long amount) {
+    	if (amount < 0) {
+    		throw new IllegalArgumentException("사용 시, amount는 음수일 수 없습니다. 충전 함수를 사용해주시기 바랍니다.");
+    	}
+    	
     	Long remains = this.point - amount;
     	
     	if (remains < 0) {
-    		throw new IllegalArgumentException("포인트는 음수가 될 수 없습니다.");
+    		throw new IllegalArgumentException("잔여 포인트가 부족합니다.");
     	}
     	
     	return new UserPoint(this.id, remains, System.currentTimeMillis());
--- src/test/java/io/hhplus/tdd/point/UserPointUnitTests.java
+++ src/test/java/io/hhplus/tdd/point/UserPointUnitTests.java
@@ -16,13 +16,13 @@ public class UserPointUnitTests {
 	
 	// 층전 테스트
 	
-	static LongStream validRange() {
+	static LongStream validChargeRange() {
         return LongStream.rangeClosed(0, 1000); // 0부터 100까지의 범위를 제공
     }
 	
 	// 정상 범주
 	@ParameterizedTest
-	@MethodSource("validRange")
+	@MethodSource("validChargeRange")
 	public void 포인트_충전_테스트(long value) {
 		// given
 		UserPoint tester = UserPoint.empty(1L);
@@ -34,7 +34,6 @@ public class UserPointUnitTests {
         assertEquals(tester.getPoint() + value, newData.getPoint(), "충전된 포인트가 올바르게 계산되어야 합니다.");
 	}
 	
-	// 최댓값 1000000일때
 	@Test
 	public void 포인트_충전_최댓값일_때() {
 		// given
@@ -44,7 +43,6 @@ public class UserPointUnitTests {
 		assertThrows(IllegalArgumentException.class, () -> tester.rechargePoint(1000000L), "100만을 넘길 수 없습니다.");
 	}
 	
-	// 최댓값을 넘겼을 때
 	@Test
 	public void 포인트_충전_최댓값을_넘길_때() {
 		// given
@@ -54,13 +52,51 @@ public class UserPointUnitTests {
 		assertThrows(IllegalArgumentException.class, () -> tester.rechargePoint(999999999L), "100만을 넘길 수 없습니다.");
 	}
 	
-	// 음수가 들어왔을 때
 	@Test
 	public void 포인트_충전에_음수가_들어왔을_때() {
 		// given
 		UserPoint tester = UserPoint.empty(1L);
 		
 		// when & then
-		assertThrows(IllegalArgumentException.class, () -> tester.rechargePoint(-1L), "100만을 넘길 수 없습니다.");
+		assertThrows(IllegalArgumentException.class, () -> tester.rechargePoint(-1L), "음수가 들어올 수 없습니다.");
+	}
+	
+	
+	// 사용 테스트
+	
+	static LongStream validUseRange() {
+        return LongStream.rangeClosed(0, 1000); // 0부터 100까지의 범위를 제공
+    }
+	
+	// 정상 범주
+	@ParameterizedTest
+	@MethodSource("validUseRange")
+	public void 포인트_사용_테스트(long value) {
+		// given
+		UserPoint tester = new UserPoint(1L, 10000L, System.currentTimeMillis());
+		
+		// when
+		UserPoint newData = tester.usePoint(value);
+		
+		// then
+        assertEquals(tester.getPoint() - value, newData.getPoint(), "사용된 포인트가 올바르게 계산되어야 합니다.");
+	}
+	
+	@Test
+	public void 가진_포인트보다_많이_사용할_때() {
+		// given
+		UserPoint tester = new UserPoint(1L, 10000L, System.currentTimeMillis());
+		
+		// when & then
+		assertThrows(IllegalArgumentException.class, () -> tester.usePoint(1000000L), "포인트가 부족합니다.");
+	}
+	
+	@Test
+	public void 포인트_사용에_음수가_들어왔을_때() {
+		// given
+		UserPoint tester = new UserPoint(1L, 10000L, System.currentTimeMillis());
+		
+		// when & then
+		assertThrows(IllegalArgumentException.class, () -> tester.usePoint(-1L), "음수가 들어올 수 없습니다.");
 	}
 }
